86b508748aba9721a241cbef335a416714a2a57c
simbahebinbo <lansheng228@163.com>
2024-10-23 15:18:28 +0800

    升级 redis 版本和依赖包版本

--- test/test.sh
+++ test/test.sh
@@ -1,9 +1,11 @@
 #!/bin/bash
 
+IMAGE_VERSION=3.9.9-amazoncorretto-21-al2023
+
 git clone git@github.com:simbahebinbo/embedded-redis.git
 cd embedded-redis
 
-docker run -it --rm --name test-linux-amd64 -v "$(pwd)":/usr/src/mymaven -w /usr/src/mymaven maven:3.9.9 mvn clean package
+docker run -it --rm --name test-linux-amd64 -v "$(pwd)":/usr/src/mymaven -w /usr/src/mymaven maven:${IMAGE_VERSION} mvn clean package
 
 cd ..
 rm -rf embedded-redis
